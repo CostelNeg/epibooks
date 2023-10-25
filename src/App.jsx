4f812dd7090d146f67d74657db119cbdf2f25d5a
Constantin Negura <costelnegura1705@gmail.com>
2023-10-25 16:43:58 +0200

    w

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "react-circular-progressbar/dist/styles.css";
+import './App.css';
+import NavBar from './NavBar';
+
+function App() {
+return(
+
+  <>
+
+    <NavBar />
+    </>
+  
+
+)
+}
+
+export default App;
